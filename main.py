c2e613ce123f0b9e2a98bd77efd47d8a1f29a701
Jithendra Prasad <jithendra.prasad@dyocense.com>
2021-10-02 19:23:26 +0530

    model updated

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img
 from keras.layers import Conv2D, Flatten, MaxPooling2D, Dense
 from keras.models import Sequential
+from tensorflow import keras
 from PIL import Image
 import zipfile as zf
 import shutil
@@ -13,7 +14,7 @@ import glob, os, random
 files = zf.ZipFile("assets/dataset-resized.zip",'r')
 files.extractall(path='assets')
 files.close()
-time.sleep(15)
+time.sleep(10)
 base_path = 'assets/dataset-resized'
 
 img_list = glob.glob(os.path.join(base_path, '*/*.jpg'))
@@ -71,35 +72,35 @@ labels = dict((v,k) for k,v in labels.items())
 
 print(labels)
 
+if os.path.exists('data/my_model') == False:
+    model = Sequential([
+        Conv2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(300, 300, 3)),
+        MaxPooling2D(pool_size=2),
 
-model = Sequential([
-    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(300, 300, 3)),
-    MaxPooling2D(pool_size=2),
+        Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+        MaxPooling2D(pool_size=2),
 
-    Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
-    MaxPooling2D(pool_size=2),
+        Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
+        MaxPooling2D(pool_size=2),
 
-    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
-    MaxPooling2D(pool_size=2),
+        Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
+        MaxPooling2D(pool_size=2),
 
-    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
-    MaxPooling2D(pool_size=2),
+        Flatten(),
 
-    Flatten(),
+        Dense(64, activation='relu'),
 
-    Dense(64, activation='relu'),
+        Dense(6, activation='softmax')
+    ])
 
-    Dense(6, activation='softmax')
-])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    model.summary()
 
-model.summary()
+    model.fit_generator(train_generator, epochs=1, validation_data=validation_generator)
 
-model.fit_generator(train_generator, epochs=1, validation_data=validation_generator)
-
-
-
+    model.save("data/my_model")
+model = keras.models.load_model('data/my_model')
 test_x, test_y = validation_generator.__getitem__(1)
 
 preds = model.predict(test_x)
@@ -119,5 +120,6 @@ for i in range(16):
 import pandas as pd
 df = pd.DataFrame(predicted, columns=["predicted"])
 df["actual"] = actual
-df
+
+df.to_csv('assets/output.csv')
 shutil.rmtree('assets/dataset-resized')
